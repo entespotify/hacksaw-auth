65cd9cd59837ccc372353c91eea5a2a4dfaefe0b
ankit <ankithss774@gmail.com>
2025-12-15 00:46:15 +0530

    Making app browser router friendly

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "@entespotify/react-oauth-client-components";
 
 import "./App.css";
 import { ThemeProvider } from "./theme/themeProvider";
 import AppResourceRouter from "./AppResourceRouter";
+import { ROUTER_BASE_NAME } from "./services/constants";
 
 const authConfig = {
     clientId: process.env.REACT_APP_CLIENT_ID || "",
     authorizationEndpoint: process.env.REACT_APP_BASE_URL + "/o/authorize/",
     tokenEndpoint: process.env.REACT_APP_BASE_URL + "/o/token/",
-    redirectUri: window.location.origin + window.location.pathname,
+    redirectUri: window.location.origin + `/${ROUTER_BASE_NAME}/auth/callback`,
     scope: "read",
     storage: "localStorage" as const,
 };
@@ -18,11 +19,11 @@ function App() {
     return (
         <div className="App">
             <AuthProvider config={authConfig}>
-                <HashRouter>
+                <BrowserRouter basename={ROUTER_BASE_NAME}>
                     <ThemeProvider>
                         <AppResourceRouter />
                     </ThemeProvider>
-                </HashRouter>
+                </BrowserRouter>
             </AuthProvider>
         </div>
     );
--- src/services/constants.ts
+++ src/services/constants.ts
@@ -2,3 +2,5 @@ export const NAV_PORTION_OF_VH = 8;
 
 // export const BASE_URL = "http://localhost:4000";
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+export const ROUTER_BASE_NAME = process.env.REACT_APP_ROUTER_BASE_NAME || '';
